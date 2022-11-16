25641baf3d5d7ab7c4c3b25f877b27c2abdc00af
Kimberlee Ashmore <kimashmore13@gmail.com>
2022-11-16 00:31:12 -0600

    inclusion of moods to dict

--- simple_site/Program.cs
+++ simple_site/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace mood_emulator
 {
@@ -7,8 +8,51 @@ namespace mood_emulator
     {
         static public string mood_evaluator(string mood)
         {
+            mood = mood.ToLower();
+            int mood_val = 0;
+            Dictionary<string, int> Mood_dict = new Dictionary<string, int>
+            {
+                {"happy", 1 },
+                {"sad", 2},
+                {"mad", 3 },
+                {"excited", 4},
+                {"nervous", 5},
+            };
+
+            // TODO: EMULATE MORE MOODS AND TAKE IN FROM COMMAND LINE IF NOT EXISTING IN SWITCH
+            if (Mood_dict.ContainsKey(mood) == true)
+            {
+                mood_val = Mood_dict[mood];
+            }
+
+            else
+            {
+                Console.WriteLine("I've never heard of that before. could you tell me more about it?");
+                Mood_dict.Add(Console.ReadLine(), 0);
+            }
+
+            switch (mood_val)
+            {
+                case 1:
+                    Console.WriteLine("Fantastic! I have something for you.");
+                    break;
+                case 2:
+                    Console.WriteLine("Oh, I'm Sorry to hear that. I know just the thing to cheer you up.");
+                    break;
+                default:
+                    Console.WriteLine("Perfect! Jennny the Genie has some motivational words for you...");
+
+                    break;
+            }
+            return mood;
+
+        }
+
+        static public string Mood_quotes(string mood)
+        {
             string quote = "";
             string Author;
+
             return quote;
         }
 
@@ -17,7 +61,6 @@ namespace mood_emulator
             Console.WriteLine("Hello!I'm Jennny the Genie. How are you feeling today? ");
 
             string mood = Console.ReadLine();
-            Console.WriteLine("Perfect! Jennny the Genie has some motivational words for you...");
 
             // WORD CHECKING AND EVALUATION OF MOOD
 
@@ -31,7 +74,8 @@ namespace mood_emulator
         }
 
     }
-
 }
+
+
 
 
